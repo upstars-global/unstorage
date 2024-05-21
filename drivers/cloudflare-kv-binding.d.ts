3645fe5e53ac1d28dd048a8d3bd9a138ab86b507
@solaris <solaris@upstars.com>
2024-05-21 14:21:19 +0300

    upload dist & drivers

---
+++ drivers/cloudflare-kv-binding.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="@cloudflare/workers-types" />
+export interface KVOptions {
+    binding?: string | KVNamespace;
+    /** Adds prefix to all stored keys */
+    base?: string;
+}
+declare const _default: (opts: KVOptions) => import("..").Driver<KVOptions, KVNamespace<string>>;
+export default _default;
---
+++ drivers/cloudflare-r2-binding.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="@cloudflare/workers-types" />
+export interface CloudflareR2Options {
+    binding?: string | R2Bucket;
+    base?: string;
+}
+declare const _default: (opts: CloudflareR2Options | undefined) => import("..").Driver<CloudflareR2Options | undefined, R2Bucket>;
+export default _default;
